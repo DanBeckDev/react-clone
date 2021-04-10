f8654cc75cf979cc6f4a71bbc87cf617eecee225
Dan Beck <boreme12@gmail.com>
2021-04-10 13:46:16 +0100

    add recursive children logic

--- react-clone.js
+++ react-clone.js
@@ -1,33 +1,48 @@
 export function render(reactElement, domElement) {
-    // domElement.appendChild(reactElement);
-    // console.log('called');
-    domElement.appendChild(mountHost(reactElement));
+  // domElement.appendChild(reactElement);
+  // console.log('called');
+  domElement.appendChild(mount(reactElement));
 }
 
-export function createElement(type, props, children) {
-    return {
-        type,
-        props: {
-            ...props,
-            children,
-        },
-    };
+export function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children,
+    },
+  };
+}
+
+function mount(element) {
+  const { type } = element;
+  return typeof type === 'function' ? null : mountHost(element);
 }
 
 // handles elements with a host type
 function mountHost(reactElement) {
-    const type = reactElement.type;
-    const props = reactElement.props;
-    var children = props.children ? [props.children] : [];
+  const type = reactElement.type;
+  const props = reactElement.props;
+  const children = props.children;
+  const node = document.createElement(type);
 
-    var node = document.createElement(type);
-    console.log(node);
-    Object.keys(props).forEach(
-        propName =>
-            propName !== 'children' &&
-            node.setAttribute(propName, props[propName])
-    );
+  Object.keys(props).forEach((propName) => {
+    propName !== 'children' && node.setAttribute(propName, props[propName]);
+  });
 
-    children.forEach(childElement => node.appendChild(childElement));
-    return node;
+  children.forEach((childElement) => {
+    const childType = typeof childElement;
+
+    if (childType === 'string' || childType === 'number') {
+      node.appendChild(document.createTextNode(childElement));
+    } else {
+      const childNode = mount(childElement);
+      node.appendChild(childNode);
+    }
+  });
+  return node;
+}
+
+function createInitialDOMChildren(children) {
+  // TODO: extract logic out into function
 }
